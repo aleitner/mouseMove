3ca0791d93909f1062bb49dfb17b3e31ad99eb0d
Alexander Leitner <leitnersalex@gmail.com>
2020-08-31 14:43:44 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,21 +3,33 @@
 
 using namespace std;
 
-void GetDesktopResolution(int& horizontal, int& vertical);
-
 int main()
 {
     cout << "Starting..." << endl;
+
+    INPUT ip;
+    ip.type = INPUT_KEYBOARD;
+    ip.ki.wScan = 0;
+    ip.ki.time = 0;
+    ip.ki.dwExtraInfo = 0;
 
     int movement = 1;
 
     while (true)
     {
-        Sleep(100);
+        Sleep(1000);
 
         POINT p;
         if (GetCursorPos(&p))
         {
+            cout << "Pressing Control Key..." << endl;
+
+            ip.ki.wVk = VK_CONTROL;
+            ip.ki.dwFlags = 0; // 0 for key press
+            SendInput(1, &ip, sizeof(INPUT));
+            ip.ki.wVk = VK_CONTROL;
+            ip.ki.dwFlags = KEYEVENTF_KEYUP;
+            SendInput(1, &ip, sizeof(INPUT));
 
             cout << "Moving Cursor..." <<  movement << endl;
 
